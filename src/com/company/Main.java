f22660bcfe983ae566a2e178b146199c3b5faa0a
marc572m <marc572m@edu.easj.dk>
2021-09-14 10:18:02 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 20;
+
+            if (!(x > 0 && x < 10)) {
+                System.out.println("positive single digit number.");
+            }
+
+
+    }
+}
